da06baf6c5762e0d46425f58c71705f950dd8f72
hibikikkk <kyou9712@gmail.com>
2019-03-11 13:22:54 +0900

    プロダクトの準備・アニメーション・フォントが残ってる

--- my_site/urls.py
+++ my_site/urls.py
@@ -13,12 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.urls import path, include
 from django.contrib import admin
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('hibiki9712.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
--- static/lib/lightbox/links.php
+++ static/lib/lightbox/links.php
@@ -1,6 +1,6 @@
 <?php
   $links = array(
-    'css' => 'lib/lightbox/css/lightbox.min.css',
-    'js' => 'lib/lightbox/js/lightbox.min.js'
+    'css' => 'static/lib/lightbox/css/lightbox.min.css',
+    'js' => 'static/lib/lightbox/js/lightbox.min.js'
   );
 ?>
