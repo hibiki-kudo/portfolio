31708721a1afd691fc1e042766829039eae5c04d
hibikikkk <kyou9712@gmail.com>
2020-08-03 17:18:44 +0900

    fix: ローカル環境のsmtpのホストの設定 修正

--- my_site/settings.py
+++ my_site/settings.py
@@ -128,5 +128,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'kyou9712@gmail.com'
-EMAIL_HOST_PASSWORD = os.environ["MAIL_PASSWORD"]
+EMAIL_HOST_PASSWORD = os.environ.get("MAIL_PASSWORD") or ""
 EMAIL_USE_TLS = True
